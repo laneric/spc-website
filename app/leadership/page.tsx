a27982bae30e12229c404defcd32e3a3babbfb36
laneric <ericlan707@gmail.com>
2026-01-15 23:38:35 -0800

    update leadership page

--- app/leadership/page.tsx
+++ app/leadership/page.tsx
@@ -2,7 +2,7 @@ export default function Leadership() {
   return (
     <div style={{ height: 'calc(100vh - 200px)', width: '100%' }}>
       <iframe 
-        src="https://ripple-increase-bbd.notion.site/ebd//2e2ddfe3637f817aa55fcfa05687af77?v=2e2ddfe3637f81a2971d000c763476e6" 
+        src="https://ripple-increase-bbd.notion.site/ebd//2eaddfe3637f804b91e0fcd7745aee3f?v=2eaddfe3637f80ee9e91000cdeda645f" 
         width="100%" 
         height="100%"
         frameBorder="0" 
